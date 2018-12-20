c42af5424e4d577a44f6c5c0fef98a360e58959b
Lucas Ramos <lucas@interprise.lucaramos>
2018-12-19 22:47:49 -0200

    Correções no form de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 col-md-offset-3">
             <div class="box box-login">
                     <h1>
                         Uma nova forma<br>
                         de fechar contratos
                     </h1>
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <form class="form-horizontal form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
-                            <div class="col-md-8">
+                            <label for="email" class="col-md-3 control-label">E-Mail</label>
+                            <div class="col-md-9">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -23,9 +23,9 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-3 control-label">Senha</label>
 
-                            <div class="col-md-8">
+                            <div class="col-md-9">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -47,13 +47,14 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12 align-center">
+                                <button type="submit" class="btn btn-primary btn-login">
                                     Login
                                 </button>
-
+                            </div>
+                            <div class="col-md-12 align-center">
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                    Esqueceu a senha?
                                 </a>
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,4 @@
 
 
 Auth::routes();
-
 Route::get('/', 'HomeController@index')->name('home');
